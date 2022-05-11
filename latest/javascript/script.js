53a0fcccd0fe83f324d4cec8772154e9eb239ce1
Sajina <sajina.tv@techversantinfo.com>
2022-05-11 11:01:10 +0530

    add js file

--- latest/javascript/script.js
+++ latest/javascript/script.js
@@ -37,75 +37,109 @@ var data = [{
 }
 ];
 
-    var tag = document.createElement("div");
-    tag.setAttribute('new','set');
-    var text = document.createTextNode("");
-    tag.appendChild(text);
-    var element = document.getElementById("first");
-    element.appendChild(tag);
+//     var tag = document.createElement("div");
+//     tag.setAttribute('new','set');
+//     var text = document.createTextNode("");
+//     tag.appendChild(text);
+//     var element = document.getElementById("first");
+//     element.appendChild(tag);
 
-    var tag = document.createElement("div");
-    tag.setAttribute('new1','set');
-    var text = document.createTextNode("");
-    tag.appendChild(text);
-    var element = document.getElementById("new1");
-    element.appendChild(tag);
-
-
-
-console.log(typeof data);
-console.log(data);
+//     var tag = document.createElement("div");
+//     tag.setAttribute('new1','set');
+//     var text = document.createTextNode("");
+//     tag.appendChild(text);
+//     var element = document.getElementById("new1");
+//     element.appendChild(tag);
 
 
-var timeArray = [];
-var d = new Date();
-var h = d.getHours();
-var m = d.getMinutes();
 
-for (var i = h; i <= 24; i++) {
-   for (var j = m; j <= 59; j++) {
-       if (j % 15 === 0) {
-            j = j === 0 ? '00' : j;
-            if (i >= 12) {
-                timeArray.push((i - 12) + ':' + j + ' PM');
-            } else {
-                timeArray.push(i + ':' + j + ' AM');
-            }
-        }
-    }
-}
-console.log(timeArray);
+// console.log(typeof data);
+// console.log(data);
+
+
+// var timeArray = [];
+// var d = new Date();
+// var h = d.getHours();
+// var m = d.getMinutes();
+
+// for (var i = h; i <= 24; i++) {
+//    for (var j = m; j <= 59; j++) {
+//        if (j % 15 === 0) {
+//             j = j === 0 ? '00' : j;
+//             if (i >= 12) {
+//                 timeArray.push((i - 12) + ':' + j + ' PM');
+//             } else {
+//                 timeArray.push(i + ':' + j + ' AM');
+//             }
+//         }
+//     }
+// }
+// console.log(timeArray);
 
    
-for(i=0;i<1;i++)
+// for(i=0;i<1;i++)
+// {
+//    var tag = document.createElement("p");
+//    var text = document.createTextNode("Excercise");
+//    tag.appendChild(text);
+//    var element = document.getElementById("new");
+//    element.appendChild(tag);
+// }
+// for(i=0;i<1;i++)
+// {
+//    var tag = document.createElement("p");
+//    var text = document.createTextNode("Plan day");
+//    tag.appendChild(text);
+//    var element = document.getElementById("new");
+//    element.appendChild(tag);
+// }
+// for(i=0;i<1;i++)
+// {
+//    var tag = document.createElement("p");
+//    var text = document.createTextNode("Review yesterday's commit");
+//    tag.appendChild(text);
+//    var element = document.getElementById("new");
+//    element.appendChild(tag);
+// }
+// for(i=0;i<1;i++)
+// {
+//    var tag = document.createElement("p");
+//    var text = document.createTextNode("Code review");
+//    tag.appendChild(text);
+//    var element = document.getElementById("new");
+//    element.appendChild(tag);
+// }
+
+
+
+for(var key in data)
 {
-   var tag = document.createElement("p");
-   var text = document.createTextNode("Excercise");
-   tag.appendChild(text);
-   var element = document.getElementById("new");
-   element.appendChild(tag);
-}
-for(i=0;i<1;i++)
-{
-   var tag = document.createElement("p");
-   var text = document.createTextNode("Plan day");
-   tag.appendChild(text);
-   var element = document.getElementById("new");
-   element.appendChild(tag);
-}
-for(i=0;i<1;i++)
-{
-   var tag = document.createElement("p");
-   var text = document.createTextNode("Review yesterday's commit");
-   tag.appendChild(text);
-   var element = document.getElementById("new");
-   element.appendChild(tag);
-}
-for(i=0;i<1;i++)
-{
-   var tag = document.createElement("p");
-   var text = document.createTextNode("Code review");
-   tag.appendChild(text);
-   var element = document.getElementById("new");
-   element.appendChild(tag);
+    
+    for(var subkey in data[key])
+    {  
+        console.log(data[key][subkey]);
+        if(data[key][subkey]==0)
+        {
+            document.getElementById("example1").innerHTML = "8:00";
+            let content= document.getElementById("example2");
+            content.innerHTML="Exercise";
+            content.classList.add("color");
+        }
+        if(data[key][subkey]==25)
+        {
+            document.getElementById("example4").innerHTML = "8:30";
+            let content= document.getElementById("example5");
+            content.innerHTML="Travel to work";
+            content.classList.add("color");
+        }
+        // if(data[key][subkey]==30)
+        // {
+        //     document.getElementById("example3").innerHTML = "8:30";
+        //     let content= document.getElementById("example3");
+        //     content.innerHTML="Review yesterday's commit";
+        //     content.classList.add("color");
+        // }
+
+
+    }
 }
