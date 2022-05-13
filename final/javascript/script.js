d9753672f954906293541c04d6fa5f53499ade10
Sajina <sajina.tv@techversantinfo.com>
2022-05-13 15:56:47 +0530

    new js

--- final/javascript/script.js
+++ final/javascript/script.js
@@ -1,75 +1,3 @@
-// var data = [{
-//     start: 0,
-//     duration: 15,
-//     title: "Exercise"
-// }, {
-//     start: 25,
-//     duration: 30,
-//     title: "Travel to work"
-// }, {
-//     start: 30,
-//     duration: 30,
-//     title: "Plan day"
-// }, {
-//     start: 60,
-//     duration: 15,
-//     title: "Review yesterday's commits"
-// }, {
-//     start: 100,
-//     duration: 15,
-//     title: "Code review"
-// }, {
-//     start: 180,
-//     duration: 90,
-//     title: "Have lunch with John"
-// }, {
-//     start: 360,
-//     duration: 30,
-//     title: "Skype call"
-// }, {
-//     start: 370,
-//     duration: 45,
-//     title: "Follow up with designer"
-// }, {
-//     start: 400,
-//     duration: 30,
-//     title: "Push up branch"
-// }
-// ];
-// $(".main-container").append(`<div class="time1">
-// <div class="">
-//   <span class="time">${hour+':'+min}</span>
-//   <div class="timeContent">
-//     <div class=" contentActive firstOne">
-//       Exercise
-//     </div>
-//     <div class=" d-block h-33"></div>
-//     <div class="d-flex  h-16">
-//       <div class=" w-50"></div>
-//       <div class="contentActive w-50 ms-0"></div>
-//     </div>
-//   </div>
-// </div>
-// var s=data.length;
-// for(var i=0;i<s;i++)
-// {
-//     var title1=data[i].title;
-//     $(".main-container").append(`
-//     <div class="tab">
-//                 <form>
-//                     <span class="time1">
-//                         8:00
-//                     </span>
-//                     <input type="text" placeholder="Excercise" class="text">
-//                 </form>
-//             </div>
-//             ${title1}</div>
-//     </div>
-//     `)
-// }
-
-
-
 var data = [{
     start: 0,
     duration: 15,
@@ -126,40 +54,38 @@ function getTimeArray() {
     }
     return times;
 }
-// for(i=0;i<data.length;i++){
-// var value=0;
-// if{
-//     tempValue =data[i].start-(data[i-1].start+ data[i-1].duration);
-// }
-// $(".container1").append(`
-// <div class="color
-// style="height:
-// ${data[i].duration}px;margin-top:
-// ${value}px"; >
-// ${data[i].title}"
-
-// </div>
-// `)
-// }
-var value=0;
+var value = 0;
 for (let i = 0; i < data.length; i++) {
+    var nextGap = data[i + 1].start - (data[i].start + data[i].duration);
+    if (nextGap >= 0) {
+        var mBottom = nextGap;
+        var width = '100% ';
+        var mLeft = '0%';
+    } else {
+        mBottom = '0';
+        width = '50%';
+        mLeft = '50%';
+    }
     $("#container2").append(`
-    <div class="color">
+    <div class="color" style="height:${data[i].duration}px;margin-bottom:${mBottom}px;width:${width};margin-left:${mLeft}">
     ${data[i].title}
     </div>
 `)
 }
-// $('#container1').append(
-// <div class="color" style="${height}">${data[i].title}</div>
-// );
-// let height = "height:"+ (data[i].duration)*2+"px";
+// if(times[i]=="0:00"){
+//     times[i]=="12:00";
+// }
+// elseif(times[i]=="0:30"){
+//     times[i]=="12:30";
+// }
+// else{
+//     tt=tt+x;
+// }
+// }
 
 
-// let sample = 0;
-// if(data[i].start == 0){
-    // $("#tasks").append(`
-    //     <div class="fs-13 bodyBackgrounColor borderColor" style="${height}">${data[i].title}</div>
-    //  `);
+
+
 
 // document.getElementById('time-title').innerHTML="Time schedule";
 // document.getElementById('container1').innerHTML=data.map(item =>
@@ -177,36 +103,3 @@ for (let i = 0; i < data.length; i++) {
 // var start = dp.visibleStart();
 // var end = dp.visibleEnd();
 // dp.init();
-// $.each(data, function(i, item) {
-//     alert(item.start)
-//   });
-// function print(data)
-// $('body').append($("<div id='new'/>"));
-// $('#new').append($("<span>"+data.start[0].data.start+"   "+"</span>"));
-// $( ".container" ).append( $( "div" ) );
-// var number = 1;
-// console.log(data.length);
-// for (var i = 0; i < data.length; i++) {
-//     var duration = 0;
-//     var value = 0;
-//     datanew = 0;
-//     if (data[i].start != 0) {
-//         value = data[i].start - (data[i - 1].start + data[i - 1].duration);
-//     }
-//     // if (value < 0) {
-//     //     number++;
-//     // }
-//     $(".newdata").append(`
-//         <div class="color" style="height:
-//         ${data[i].duration}px;margin-top:
-//         ${value}px;margin-left:
-//         ${(number == 1) ? 0 : 100 / number}%">
-//         ${data[i].title}
-//         </div>`)
-//     if (number > 1) {
-//         number = number - 1;
-//     }
-//     else {
-//         datanew = 0;
-//     }
-// }
