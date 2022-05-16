844ff452993fc6c8bbc360e2d158f06764b55695
Sajina <sajina.tv@techversantinfo.com>
2022-05-16 17:16:01 +0530

    updated

--- final/javascript/script.js
+++ final/javascript/script.js
@@ -39,8 +39,19 @@ var data = [{
 $(".time-title").html("<b>Time Schedule</b>");
 $(".time-time").html("8:00 am-6:00 pm");
 var newtime = getTimeArray();
-for (i = 0; i < newtime.length; i++) {
-    $("#container1").append(`<div style="height:30px">${newtime[i]}</div>`);
+// for (i = 0; i < newtime.length; i++) {
+//     $("#container1").append(`<div style="height:30px">${newtime[i]}</div>`);
+// }
+for(i=0;i<newtime.length;i++)
+{
+    if(newtime.indexOf(newtime[i])%2==0)
+    {
+        $("#container1").append(`<div class="border time-array" >${newtime[i]}</div>`);
+    }
+    else
+    {
+        $("#container1").append(`<div class="font time-array">${newtime[i]}</div>`);
+    } 
 }
 function getTimeArray() {
     var x = 30;
@@ -63,7 +74,12 @@ var value = 0;
 for (let i = 0; i < data.length; i++) {
     if (i == data.length - 1) {
         var nextGap = data[i].start - (data[i - 1].start + data[i - 1].duration);
-    } else {
+    }
+    // else if(i<=data.length)
+    // {
+    //     var nextGap = data[i].start - (data[i - 1].start + data[i - 1].duration);
+    // }
+     else {
         nextGap = data[i + 1].start - (data[i].start + data[i].duration);
     }
     // var nextGap = data[i + 1].start - (data[i].start + data[i].duration);
@@ -79,11 +95,23 @@ for (let i = 0; i < data.length; i++) {
     if(i==data.length-2){
         var mLeft = '0%';
     }
+    
+    if(data[i].title=="Plan day")
+    {
+        width="50%";
+        $("#container2").append(`
+        <div class="color plan"style="height:${data[i].duration}px;margin-bottom:${mBottom}px;width:${width};margin-left:${mLeft};top:${data[i].start}px"">
+        ${data[i].title}
+        </div>
+    `)
+    }
+    else{
     $("#container2").append(`
     <div class="color" style="height:${data[i].duration}px;margin-bottom:${mBottom}px;width:${width};margin-left:${mLeft};position:absolute;top:${data[i].start}px">
     ${data[i].title}
     </div>
 `)
+}
 }
 
 
